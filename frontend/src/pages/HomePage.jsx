d0a0bc025346082ecf298f7bdab30838c47b51b8
Leo Leerilä <Leo.Leerila@metropolia.fi>
2026-02-11 13:48:34 +0200

    [iter2] add job fetching

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,20 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const [jobs, setJobs] = useState([]);
 
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const response = await fetch(`/api/jobs`);
+      console.log(`/api/jobs`);
+      const json = await response.json();
+
+      if (response.ok) {
+        setJobs(json);
+      }
+    };
+
+    fetchJobs();
+  }, []);
+
   return (
     <div className="home">
       <div className="job-list">
